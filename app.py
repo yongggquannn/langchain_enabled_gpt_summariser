49cb729bacec341cbf08fbf30b5604bce2175b8f
Yong Quan <tanyongquanm@gmail.com>
2023-10-22 19:46:59 +0800

    feat: added file uploader

--- app.py
+++ app.py
@@ -2,14 +2,22 @@ import os
 from apikey import apikey
 
 import streamlit as st
-from langchain.llms import openai
+from langchain.llms import OpenAI
 
 from langchain.document_loaders import PyPDFLoader
 
 os.environ['OPENAI_API_KEY'] = apikey
 
 #App framework
-st.title('🐳 LLM-Powered PDF Summarizer')
+st.set_page_config(page_title="SummarizerGPT", page_icon="📖", layout="wide")
+st.header("📖 SummarizerGPT")
 
+llm = OpenAI(temperature = 0.9)
+
+uploaded_file = st.file_uploader(
+    "Upload a pdf, docx, or txt file",
+    type=["pdf", "docx", "txt"],
+    help="Scanned documents are not supported yet!",
+)
 
 
