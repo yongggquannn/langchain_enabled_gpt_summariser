69a1d745f069f28b609a3945cf4ce46bf33012c3
Yong Quan <tanyongquanm@gmail.com>
2023-11-07 11:57:54 +0800

    chore: update to type of file parsing in

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ def main():
     with st.sidebar:
         st.subheader("Your documents")
         pdf_docs = st.file_uploader(
-            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+            "Upload your PDFs here and click on 'Process'", accept_multiple_files=False, type="pdf")
         if st.button("Process"):
             with st.spinner("Processing"):
                 # get pdf text
